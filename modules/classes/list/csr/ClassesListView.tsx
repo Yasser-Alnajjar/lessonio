b383bed759dc0b6f28a0c6beaa37617f32f7b7d8
Yasser-Alnajjar <yasseralnajjar72@gmail.com>
2026-08-20 21:53:26 +0300

    refactor: remove debug log for visibleUpcoming in ClassesListView

--- modules/classes/list/csr/ClassesListView.tsx
+++ modules/classes/list/csr/ClassesListView.tsx
@@ -112,7 +112,6 @@ export const ClassesListView = ({
   };
 
   const openCreate = () => setFormState({ open: true, item: null });
-  console.log(visibleUpcoming);
 
   return (
     <div className="flex flex-col gap-8 p-4">
